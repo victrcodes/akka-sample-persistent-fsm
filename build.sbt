9d464b2cc18efb6bb8b8441fe00c3813e5f53f0c
Vic <vykintas.rutkunas@xendpay.com>
2015-10-05 16:40:16 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,16 @@
+name := "akka-sample-persistent-fsm"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+mainClass := Some("PersistentFSMExample")
+
+fork := true
+
+libraryDependencies ++= Seq(
+	"com.typesafe.akka" %% "akka-actor" % "2.4.0",
+	"com.typesafe.akka" %% "akka-persistence" % "2.4.0",
+	"org.iq80.leveldb" % "leveldb" % "0.7",
+	"org.fusesource.leveldbjni" % "leveldbjni-all" % "1.8"
+)
---
+++ src/main/scala/PersistentFSMExample.scala
@@ -0,0 +1,98 @@
+import akka.actor.{ActorSystem, Props}
+import akka.pattern.ask
+import akka.persistence.fsm.PersistentFSM
+import akka.persistence.fsm.PersistentFSM.FSMState
+import akka.util.Timeout
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.reflect._
+import scala.util.Random
+
+final case class SetNumber(num: Integer)
+final case class Reset()
+
+sealed trait State extends FSMState
+case object Idle extends State {
+	override def identifier: String = "Idle"
+}
+case object Active extends State {
+	override def identifier: String = "Active"
+}
+
+sealed trait Data {
+	def add(number: Integer): Data
+	def empty(): Data
+}
+case object Empty extends Data {
+	def add(number: Integer) = Numbers(Vector(number))
+	def empty() = this
+}
+final case class Numbers(queue: Seq[Integer]) extends Data {
+	def add(number: Integer) = Numbers(queue :+ number)
+	def empty() = Empty
+}
+
+sealed trait DomainEvt
+case class SetNumberEvt(num: Integer) extends DomainEvt
+case class ResetEvt() extends DomainEvt
+
+class Generator extends PersistentFSM[State, Data, DomainEvt] {
+
+	override def applyEvent(domainEvent: DomainEvt, currentData: Data): Data = {
+		domainEvent match {
+			case SetNumberEvt(num) => currentData.add(num)
+			case ResetEvt() => currentData.empty()
+		}
+	}
+
+	override def persistenceId: String = "generator"
+
+	override def domainEventClassTag: ClassTag[DomainEvt] = classTag[DomainEvt]
+
+	startWith(Idle, Empty)
+
+	when(Idle) {
+		case Event(SetNumber(num), _) =>
+			println("Starting idle")
+			goto(Active) applying SetNumberEvt(num)
+		case Event(Reset, _) =>
+			println("Reset")
+			goto(Active) applying ResetEvt()
+	}
+
+	when(Active) {
+		case Event(SetNumber(num), numbers: Data) =>
+			println(numbers)
+			stay applying SetNumberEvt(num)
+		case Event(Reset, _) =>
+			println("Reset")
+			stay applying ResetEvt() replying "reset done"
+	}
+
+	initialize()
+
+}
+
+object PersistentFSMExample extends App {
+
+	val system = ActorSystem()
+
+	val actor = system.actorOf(Props[Generator])
+
+ 	implicit val timeout = Timeout(500 millis)
+
+	val reset: Future[_] = if (args.length > 0 && args(0) == "reset") actor ? Reset else Future("continue")
+
+	reset.onComplete { _ =>
+		actor ! SetNumber(Random.nextInt())
+		actor ! SetNumber(Random.nextInt())
+		actor ! SetNumber(Random.nextInt())
+		actor ! SetNumber(Random.nextInt())
+		actor ! SetNumber(Random.nextInt())
+	}
+
+	Thread.sleep(1000)
+	system.terminate()
+
+}
